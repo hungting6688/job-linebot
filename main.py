0df29313d6cda202f7c6eb605a59a7f323d681e8
hungting6688 <85077661+hungting6688@users.noreply.github.com>
2025-04-25 12:32:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,4 +44,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
+
